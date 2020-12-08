15134ad09335a6a1eb2b154753f706ef3c9e07e5
gauravkgs1620 <gauravkgs1620@gmail.com>
2020-12-08 15:25:04 -0800

    Initial Commit Fun

---
+++ a4mee-api/src/main/java/com/scholar/a4mee/A4meeApplication.java
@@ -0,0 +1,13 @@
+package com.scholar.a4mee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A4meeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A4meeApplication.class, args);
+	}
+
+}
